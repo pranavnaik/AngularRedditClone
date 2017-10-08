00851bb47d1b70b477557d2e49ff09ee80c56653
pranavnaik_ <pranav@singulerp.com>
2017-10-08 19:05:42 +0530

    Reddit clone using angular4, bootstrap and fontawesome
    Reddit clone using angular4, bootstrap and fontawesome

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Article } from "./article/article.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+ articles : Article[];
+
+ constructor() {
+    this.articles = [
+      new Article('TypeScript', 'http://typescriptlang.org', 10),
+      new Article('Angular Homepage', 'http://angular.io', 6),
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),  
+    ];
+}
+
+
+addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+}
+
+sortArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+}
+
+}
